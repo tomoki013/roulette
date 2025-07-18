16eb7de2dcc7e60706d187ad776d93585c9cd1ad
tomoki013 <tomoki_ttttt@icloud.com>
2025-07-18 14:22:35 +0900

    各種Googleタグの追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import "./globals.css";
 
 export default function RootLayout({
@@ -7,6 +8,26 @@ export default function RootLayout({
 }>) {
   	return (
   	  	<html>
+			<head>
+
+				{/* Google Search Console */}
+				<meta name="google-site-verification" content="qd9h_oeUkXKK0F-u4U5Z-c540MUq_Agst3K0rF8ERdM" />
+				{/* Google Adsense */}
+				<meta name="google-adsense-account" content="ca-pub-8687520805381056" />
+				{/* Google Analytics */}
+				{/* Google tag (gtag.js) */}
+				<Script async src="https://www.googletagmanager.com/gtag/js?id=G-L4CDHVGSSL"></Script>
+				<Script id="google-analytics">
+				  	{`
+				  		window.dataLayer = window.dataLayer || [];
+				  		function gtag(){dataLayer.push(arguments);}
+				  		gtag('js', new Date());
+						
+				  		gtag('config', 'G-L4CDHVGSSL');
+				  	`}
+				</Script>
+
+			</head>
   	  	  	<body>{children}</body>
   	  	</html>
   	);
