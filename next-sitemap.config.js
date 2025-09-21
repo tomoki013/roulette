b1fc245977b27099d996c930546e70df42126406
tomoki013 <tomoki_ttttt@icloud.com>
2025-09-21 20:32:23 +0900

    設定の追加

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://webroulette.netlify.app", // あなたのサイトのURL
+  generateRobotsTxt: true, // robots.txtも自動生成する場合
+  sitemapSize: 7000, // 1つのサイトマップファイルに含めるURLの最大数
+  outDir: "./public", // App Routerのルートに出力
+};
